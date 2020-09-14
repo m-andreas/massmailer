67a2d102126aba0c6b6abfd71040edfe6178c7dd
m-andreas <mar.andreas@yahoo.de>
2020-09-14 23:12:02 +0200

    Add Error to Api Response

--- src/main/java/com/markus/MassMailer/api/ApiError.java
+++ src/main/java/com/markus/MassMailer/api/ApiError.java
@@ -3,8 +3,15 @@ package com.markus.MassMailer.api;
 import org.springframework.http.HttpStatus;
 
 public class ApiError extends ApiResponse{
+    private String error;
+
     public ApiError(HttpStatus status, String message, String error) {
         this.setStatus(status);
         this.setMessage(message);
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
     }
 }
